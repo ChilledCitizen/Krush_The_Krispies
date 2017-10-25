6395ae8ce8694a89cdf20c23e6e0c2fcb851e8c4
Casimir Kuusela <crk.kuusela@gmail.com>
2017-10-25 12:28:24 +0300

    Added death

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Vector3 startPos, lastVelocity, accerelation;
+    public float killZone, health = 100;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        accerelation = (rb.velocity - lastVelocity) / Time.fixedDeltaTime;
+        lastVelocity = rb.velocity;
+
+
+        if (accerelation.magnitude > killZone)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.GetComponent<Rigidbody>() == null)
+        {
+            return;
+        }
+
+        if (other.gameObject.tag == "Ammo")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            float damage = other.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 10;
+            health -= damage;
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
